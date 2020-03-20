759bb2e5564d1c6277e3aff312d8dbf6c3d81202
LeahXULiu <58391184+Leahxuliu@users.noreply.github.com>
2020-03-20 20:47:28 +0900

    XU

--- Graph/323.Number of Connected Components in an Undirected Graph.py
+++ Graph/323.Number of Connected Components in an Undirected Graph.py
@@ -9,17 +9,92 @@
 
 
 2. 理解：
-
+无向图
+判断有几个群
 
 3. 题目类型：
-
+无向图
+判断孤立和分群
 
 4. 输出输入以及边界条件：
-input: 
-output: 
-corner case: 
+input: n: int, edges: List[List[int]]
+output: int
+corner case: n == 0 --> 0
+             edges == [] --> n
 
 5. 解题思路
+    方法一：利用并查集对nodes进行分组
+    方法二：dfs [0,1]法
     
+'''
+'''
+union-find
+易错点：有环的时候，groupTag里面数的个数不等于群的个数
+要用一个numb来表示
+具体原因见‘并查集.py'
+'''
+
+class Solution:
+    def countComponents(self, n, edges):
+        if n == 0:
+            return 0
+        if edges == []:
+            return n
+        
+        groupTag = [i for i in range(n)]
+        
+        def find(index):
+            if groupTag[index] == index:
+                return index
+            else:
+                return find(groupTag[index])
+            
+        numb = 0
+        for i,j in edges:
+            root1 = find(i)
+            root2 = find(j)
+            
+            if root1 == root2:
+                pass
+            else:
+                numb += 1
+                groupTag[root2] = root1
+        print(groupTag)        
+        return n - numb
+
 
 '''
+DFS
+'''
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[] for _ in range(n)]
+        visited = [0] * n
+        
+        for i,j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+            
+        def dfs(index):
+            if visited == [1] * n:
+                return 
+            visited[index] = 1
+            for out_node in graph[index]:
+                if visited[out_node] == 0:
+                    dfs(out_node)
+            
+        group = 0
+        for i in range(n):
+            if visited[i] == 0:
+                dfs(i)
+                group += 1
+        
+        return group
+
+
+
+x = Solution()
+print(x.countComponents(4,[[2,3],[1,2],[1,3]]))
+
+
--- Graph/并查集.py
+++ Graph/并查集.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# union-find method
 # 用并查集判断有几类
 # 并查集的核心：归类
 # 比如 pairs [[0,1],[1,2],[3,4]]
@@ -16,6 +17,7 @@ class Solution:
         
         # Union操作，进来的这两个节点是联通的，union函数是将这两个节点合并成一个组。通过调用find操作，对两个节点找到其祖先
         # 然后如果祖先相同的话，证明这两个节点已经再一个组里，跳过；如果不是的话，将第二个节点的祖先设置为第一个节点
+        connect = 0
         for i, j in pairs:
             root1 = self.find(i, groupTag)
             root2 = self.find(j, groupTag)
@@ -24,8 +26,8 @@ class Solution:
                 pass
             else:
                 groupTag[root2] = root1  # 连接新点
-
-        return len(set(groupTag))
+                connect += 1  # 扩展次数，不包括环的 比如 0-1-2， 三个数，两次扩展
+        return n - connect  # 群的个数 = n - 扩展次数； 因为当所有node都相连（不包含环）需要n-1个扩展线，也就是connct的值，没增加一个群，也就是减少一个connect
         
 
     # find操作是为每一个节点找到它的最远祖先，如果节点对应的数值为初始的index的话，证明其依然为isolate，
